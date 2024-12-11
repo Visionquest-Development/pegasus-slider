fa5e979888b308a3a92fbfb1ca6f7ee37fab47db
Jim O'Brien <jim.obrien3@gmail.com>
2024-12-11 00:52:22 -0500

    updating pegasus-slider plugin: usage page when not pegasus theme

--- pegasus-slider.php
+++ pegasus-slider.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /*
 Plugin Name: Pegasus Slider Plugin
 Plugin URI:  https://developer.wordpress.org/plugins/the-basics/
@@ -19,71 +19,246 @@ Domain Path: /languages
 		exit;
 	}
 
-	function pegasus_slider_menu_item() {
-		//add_menu_page("Slider", "Slider", "manage_options", "pegasus_slider_plugin_options", "pegasus_slider_plugin_settings_page", null, 99);
-		//add_submenu_page("pegasus_slider_plugin_options", "Shortcode Usage", "Usage", "manage_options", "pegasus_slider_plugin_shortcode_options", "pegasus_slider_plugin_shortcode_settings_page" );
-	}
-	add_action("admin_menu", "pegasus_slider_menu_item");
 
-	function pegasus_slider_plugin_settings_page() { ?>
-	    <div class="wrap">
-	    <h1>Slider</h1>
-		
-		<form method="post" action="options.php">
-	        <?php
-	            settings_fields("section");
-	            do_settings_sections("theme-options");      
-	            submit_button(); 
-	        ?>          
-	    </form>
-		
+	function pegasus_slider_check_main_theme_name() {
+		$current_theme_slug = get_option('stylesheet'); // Slug of the current theme (child theme if used)
+		$parent_theme_slug = get_option('template');    // Slug of the parent theme (if a child theme is used)
+
+		//error_log( "current theme slug: " . $current_theme_slug );
+		//error_log( "parent theme slug: " . $parent_theme_slug );
+
+		if ( $current_theme_slug == 'pegasus' ) {
+			return 'Pegasus';
+		} elseif ( $current_theme_slug == 'pegasus-child' ) {
+			return 'Pegasus Child';
+		} else {
+			return 'Not Pegasus';
+		}
+	}
+
+	function pegasus_pegasus_slider_menu_item() {
+		if ( pegasus_slider_check_main_theme_name() == 'Pegasus' || pegasus_slider_check_main_theme_name() == 'Pegasus Child' ) {
+			//do nothing
+		} else {
+			//echo 'This is NOT the Pegasus theme';
+			add_menu_page(
+				"Slider", // Page title
+				"Slider", // Menu title
+				"manage_options", // Capability
+				"pegasus_pegasus_slider_plugin_options", // Menu slug
+				"pegasus_pegasus_slider_plugin_settings_page", // Callback function
+				null, // Icon
+				92 // Position in menu
+			);
+		}
+	}
+	add_action("admin_menu", "pegasus_pegasus_slider_menu_item");
+
+	function pegasus_pegasus_slider_plugin_settings_page() { ?>
+	    <div class="wrap pegasus-wrap">
+			<h1>Pegasus Slider Usage</h1>
+
+			<div>
+				<h3>Pegasus Slider Usage 1:</h3>
+				<style>
+					pre {
+						background-color: #f9f9f9;
+						border: 1px solid #aaa;
+						page-break-inside: avoid;
+						font-family: monospace;
+						font-size: 15px;
+						line-height: 1.6;
+						margin-bottom: 1.6em;
+						max-width: 100%;
+						overflow: auto;
+						padding: 1em 1.5em;
+						display: block;
+						word-wrap: break-word;
+					}
+
+					input[type="text"].code {
+						width: 100%;
+					}
+				</style>
+				<pre >[slider]
+	[slide class="testing"]
+		<?php echo htmlspecialchars('<img class="alignnone size-full wp-image-12" src="https://via.placeholder.com/960x550/" alt="Gold-and-Black-Logo">'); ?>
+
+	[/slide]
+	[slide]
+		<?php echo htmlspecialchars('<img class="alignnone size-full wp-image-12" src="https://via.placeholder.com/600x350/" alt="Gold-and-Black-Logo">'); ?>
+
+	[/slide]
+[/slider]</pre>
+
+				<input
+					type="text"
+					readonly
+					value="<?php echo esc_html('[slider][slide class="testing"]<img class="alignnone size-full wp-image-12" src="https://via.placeholder.com/960x550/" alt="Gold-and-Black-Logo">[/slide][slide]<img class="alignnone size-full wp-image-12" src="https://via.placeholder.com/600x350/" alt="Gold-and-Black-Logo">[/slide][/slider]'); ?>"
+					class="regular-text code"
+					id="my-shortcode"
+					onClick="this.select();"
+				>
+			</div>
+
+			<div>
+				<h3>Pegasus News Slider Usage 1:</h3>
+				<style>
+					pre {
+						background-color: #f9f9f9;
+						border: 1px solid #aaa;
+						page-break-inside: avoid;
+						font-family: monospace;
+						font-size: 15px;
+						line-height: 1.6;
+						margin-bottom: 1.6em;
+						max-width: 100%;
+						overflow: auto;
+						padding: 1em 1.5em;
+						display: block;
+						word-wrap: break-word;
+					}
+
+					input[type="text"].code {
+						width: 100%;
+					}
+				</style>
+				<pre >[news_slider the_query="showposts=100&post_type=post"]</pre>
+
+				<input
+					type="text"
+					readonly
+					value="<?php echo esc_html('[news_slider the_query="showposts=100&post_type=post"]'); ?>"
+					class="regular-text code"
+					id="my-shortcode"
+					onClick="this.select();"
+				>
+			</div>
+
+			<div>
+				<h3>Pegasus Thumbnail Slider Usage 1:</h3>
+				<style>
+					pre {
+						background-color: #f9f9f9;
+						border: 1px solid #aaa;
+						page-break-inside: avoid;
+						font-family: monospace;
+						font-size: 15px;
+						line-height: 1.6;
+						margin-bottom: 1.6em;
+						max-width: 100%;
+						overflow: auto;
+						padding: 1em 1.5em;
+						display: block;
+						word-wrap: break-word;
+					}
+
+					input[type="text"].code {
+						width: 100%;
+					}
+				</style>
+				<pre >[thumb_slider]
+	[thumb_slide title="slide1" number="1"]
+		<?php echo htmlspecialchars('<img src="http://slippry.com/assets/img/image-1.jpg" alt="This is caption 1">'); ?>
+
+	[/thumb_slide]
+	[thumb_slide title="slide2" number="2"]
+		<?php echo htmlspecialchars('<img src="http://slippry.com/assets/img/image-2.jpg" alt="This is caption 2">'); ?>
+
+	[/thumb_slide]
+	[thumb_slide title="slide3" number="3"]
+		<?php echo htmlspecialchars('<img src="http://slippry.com/assets/img/image-3.jpg" alt="This is caption 3">'); ?>
+
+	[/thumb_slide]
+	[thumb_slide title="slide4" number="4"]
+		<?php echo htmlspecialchars('<img src="http://slippry.com/assets/img/image-4.jpg" alt="This is caption 4">'); ?>
+
+	[/thumb_slide]
+[/thumb_slider]</pre>
+
+				<input
+					type="text"
+					readonly
+					value="<?php echo esc_html('[thumb_slider][thumb_slide title="slide1" number="1"]<img src="http://slippry.com/assets/img/image-1.jpg" alt="This is caption 1">[/thumb_slide][thumb_slide title="slide2" number="2"]<img src="http://slippry.com/assets/img/image-2.jpg" alt="This is caption 2">[/thumb_slide][thumb_slide title="slide3" number="3"]<img src="http://slippry.com/assets/img/image-3.jpg" alt="This is caption 3">[/thumb_slide][thumb_slide title="slide4" number="4"]<img src="http://slippry.com/assets/img/image-4.jpg" alt="This is caption 4">[/thumb_slide][/thumb_slider]'); ?>"
+					class="regular-text code"
+					id="my-shortcode"
+					onClick="this.select();"
+				>
+			</div>
+
+			<p style="color:red;">MAKE SURE YOU DO NOT HAVE ANY RETURNS OR <?php echo htmlspecialchars('<br>'); ?>'s IN YOUR SHORTCODES, OTHERWISE IT WILL NOT WORK CORRECTLY</p>
+
 		</div>
 	<?php
 	}
-	
+
+
+	//function pegasus_slider_menu_item() {
+		//add_menu_page("Slider", "Slider", "manage_options", "pegasus_slider_plugin_options", "pegasus_slider_plugin_settings_page", null, 99);
+		//add_submenu_page("pegasus_slider_plugin_options", "Shortcode Usage", "Usage", "manage_options", "pegasus_slider_plugin_shortcode_options", "pegasus_slider_plugin_shortcode_settings_page" );
+	//}
+	//add_action("admin_menu", "pegasus_slider_menu_item");
+	/*
+	function pegasus_slider_plugin_settings_page() { ?>
+	    <div class="wrap">
+	    <h1>Slider</h1>
+
+		<form method="post" action="options.php">
+	        <?php
+	            settings_fields("section");
+	            do_settings_sections("theme-options");
+	            submit_button();
+	        ?>
+	    </form>
+
+		</div>
+	<?php
+	}
+	*/
+	/*
 	function pegasus_slider_plugin_shortcode_settings_page() { ?>
 		<div class="wrap pegasus-wrap">
 			<h1>Shortcode Usage</h1>
 			<p>Slider Usage: <pre>[slider][slide class="testing"]<?php echo htmlspecialchars('<img class="alignnone size-full wp-image-12" src="http://www.fillmurray.com/960/550" alt="Gold-and-Black-Logo">'); ?>[/slide][slide]<?php echo htmlspecialchars('<img class="alignnone size-full wp-image-12" src="http://www.fillmurray.com/600/350" alt="Gold-and-Black-Logo">'); ?>[/slide][/slider]</pre></p>
 			<p>Post Slider Usage: <pre>[news_slider the_query="showposts=100&post_type=post"]</pre> </p>
 			<p>Thumb Slider Usage: <pre>[thumb_slider][thumb_slide title="slide1" number="1"]<?php echo htmlspecialchars('<img src="http://slippry.com/assets/img/image-1.jpg" alt="This is caption 1">'); ?>[/thumb_slide][thumb_slide title="slide2" number="2"]<?php echo htmlspecialchars('<img src="http://slippry.com/assets/img/image-2.jpg" alt="This is caption 2">'); ?>[/thumb_slide][thumb_slide title="slide3" number="3"]<?php echo htmlspecialchars('<img src="http://slippry.com/assets/img/image-3.jpg" alt="This is caption 3">'); ?>[/thumb_slide][thumb_slide title="slide4" number="4"]<?php echo htmlspecialchars('<img src="http://slippry.com/assets/img/image-4.jpg" alt="This is caption 4">'); ?>[/thumb_slide][/thumb_slider]</pre></p>
-			
-			
+
+
 			<p style="color:red;">MAKE SURE YOU DO NOT HAVE ANY RETURNS OR <?php echo htmlspecialchars('<br>'); ?>'s IN YOUR SHORTCODES, OTHERWISE IT WILL NOT WORK CORRECTLY</p>
-		
+
 		</div>
 		<?php
 	}
-	
-	
-	
+	*/
+
+
 	function pegasus_slider_plugin_styles() {
 
 		wp_register_style( 'slippery-css', trailingslashit( plugin_dir_url( __FILE__ ) ) . 'css/slippery.css', array(), null, 'all' );
 		wp_register_style( 'slippery-slider-css', trailingslashit( plugin_dir_url( __FILE__ ) ) . 'css/slippery-slider.css', array(), null, 'all' );
-		
+
 	}
 	add_action( 'wp_enqueue_scripts', 'pegasus_slider_plugin_styles' );
-	
+
 	/**
-	* Proper way to enqueue JS 
+	* Proper way to enqueue JS
 	*/
 	function pegasus_slider_plugin_js() {
 
 		wp_register_script( 'slippery-js', trailingslashit( plugin_dir_url( __FILE__ ) ) . 'js/slippery.js', array( 'jquery' ), null, 'all' );
 		wp_register_script( 'pegasus-slider-plugin-js', trailingslashit( plugin_dir_url( __FILE__ ) ) . 'js/plugin.js', array( 'jquery' ), null, 'all' );
-		
+
 	} //end function
 	add_action( 'wp_enqueue_scripts', 'pegasus_slider_plugin_js' );
-	
-	
-	
-	
-	
+
+
+
+
+
 	/*~~~~~~~~~~~~~~~~~~~~
 		SLIDER / carousel
 	~~~~~~~~~~~~~~~~~~~~~*/
-	
+
 	/**
 	* Slider Short Code
 	*/
@@ -127,7 +302,7 @@ Domain Path: /languages
 		}
 		new SliderClass;
 	}
-	
+
 	/*~~~~~~~~~~~~~~~~~~~~
 		THUMB SLIDER / carousel
 	~~~~~~~~~~~~~~~~~~~~~*/
@@ -177,18 +352,18 @@ Domain Path: /languages
 		}
 		new ThumbSliderClass;
 	}
-	
-	
+
+
 	/*~~~~~~~~~~~~~~~~~~~~
 		News Slider shortcode
 	~~~~~~~~~~~~~~~~~~~~~*/
-	// [news_slider the_query="showposts=100&post_type=page&post_parent=453" bkg_color="#dedede" ]	
+	// [news_slider the_query="showposts=100&post_type=page&post_parent=453" bkg_color="#dedede" ]
 	function news_slider_query_shortcode($atts) {
 
 		$a = shortcode_atts( array(
 			//"bkg_color" => ''
 		), $atts );
-		
+
 		// Defaults
 		extract(shortcode_atts(array(
 			"the_query" => '',
@@ -206,8 +381,32 @@ Domain Path: /languages
 			return chr( $matches[1] );
 		}, $the_query );
 
-		// query is made               
-		query_posts($the_query);
+		$query_args = array(
+			'post_type' => 'post', // Ensure you are querying the correct post type
+			'posts_per_page' => -1, // Set the number of posts to retrieve
+			//'post_status' => 'publish', // Ensure only published posts are retrieved
+			//'category_name' => 'your-category-slug', // Optional: Filter by category
+			//'orderby' => 'date', // Optional: Order by date
+			//'order' => 'DESC' // Optional: Order descending
+		);
+
+		// echo '<pre>';
+		// var_dump( $the_query );
+		// echo '</pre>';
+		// echo '<pre>';
+		// var_dump( $query_args );
+		// echo '</pre>';
+		// Convert query string into array for WP_Query
+		parse_str( $the_query, $query_args );
+		// echo '<pre>';
+		// var_dump( $query_args );
+		// echo '</pre>';
+		// Create a new WP_Query instance
+		$query = new WP_Query( $query_args );
+
+
+		// query is made
+		//query_posts($the_query);
 
 		// Reset and setup variables
 		$output = '';
@@ -221,57 +420,63 @@ Domain Path: /languages
 		global $post;
 
 		// the loop
-		if (have_posts()) : while (have_posts()) : the_post();
+		if ($query->have_posts()) {
+			while ($query->have_posts()) {
+				$query->the_post();
 
-			$temp_title = get_the_title($post->ID);
-			$temp_link = get_permalink($post->ID);
-			$temp_date = get_the_date($post->ID);
-			$temp_pic = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
-			if ( !$temp_pic ) {
-				$temp_pic = get_template_directory_uri() . '/images/not-available.jpg';
-			}
-			$temp_excerpt = wp_trim_words( get_the_excerpt(), 150 );
-			$temp_content = wp_trim_words( get_the_content(), 300 );
-			$the_id = get_the_ID();
-			
+				$the_id = get_the_ID();
+				$temp_title = get_the_title($post->ID);
+				$temp_link = get_permalink($post->ID);
+				$temp_date = get_the_date($post->ID);
+				$temp_pic = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
+				if ( !$temp_pic ) {
+					$temp_pic = get_template_directory_uri() . '/images/not-available.jpg';
+				}
+				$temp_excerpt = wp_trim_words( get_the_excerpt(), 150 );
+				$temp_content = wp_trim_words( get_the_content(), 300 );
 
-			// output all findings - CUSTOMIZE TO YOUR LIKING
-			$output .= "<article>"; 
-				$output .= "<div class='post-$the_id text-content'>";
-				
-					$output .= "<a href='$temp_link'><h2>$temp_title</h2></a>";
-					$output .= "<i>$temp_date</i>";
-					$output .= "<br>";
-					$output .= "<p class='post-content'>";
-					
-					if(isset($temp_excerpt)) {
-						$temporary_excerpt = substr(strip_tags($temp_excerpt), 0, 150);
-						if($temporary_excerpt){
-								$output .= $temporary_excerpt; 
-								$output .= '...';
+
+
+				// output all findings - CUSTOMIZE TO YOUR LIKING
+				$output .= "<article>";
+					$output .= "<div class='post-$the_id text-content'>";
+
+						$output .= "<a href='$temp_link'><h2>$temp_title</h2></a>";
+						$output .= "<i>$temp_date</i>";
+						$output .= "<br>";
+						$output .= "<p class='post-content'>";
+
+						if(isset($temp_excerpt)) {
+							$temporary_excerpt = substr(strip_tags($temp_excerpt), 0, 150);
+							if($temporary_excerpt){
+									$output .= $temporary_excerpt;
+									$output .= '...';
+							}
+						}else{
+							$more = 0;
+							$temporary = substr(strip_tags($temp_content), 0, 300);
+							if($temporary){ $output .= $temporary; $output .= '...'; }
 						}
-					}else{  
-						$more = 0; 
-						$temporary = substr(strip_tags($temp_content), 0, 300); 
-						if($temporary){ $output .= $temporary; $output .= '...'; } 
-					}
-					$output .= "</p>";
-					$output .= "<a class='read-more-link clearfix' href='$temp_link'>Read More</a>";
-				$output .= "</div>";
-
-				if($temp_pic) { 
-					$output .= "<div class='image-content'>";
-					$output .= "<img class='post-img-feat' src='$temp_pic'>"; 
+						$output .= "</p>";
+						$output .= "<a class='read-more-link' href='$temp_link'>Read More</a>";
 					$output .= "</div>";
-				} 
-				
-				
-			$output .= "</article>";
-		endwhile; else:
-			$output .= "nothing found.";
-		endif;
 
-		wp_reset_query();
+					if($temp_pic) {
+						$output .= "<div class='image-content'>";
+						$output .= "<img class='post-img-feat' src='$temp_pic'>";
+						$output .= "</div>";
+					}
+
+
+				$output .= "</article>";
+				}//end while
+				//wp_reset_postdata();
+			} else {
+				echo '<p>No posts found.</p>';
+			}
+
+		wp_reset_postdata();
+		//wp_reset_query();
 
 		wp_enqueue_style( 'slippery-css' );
 		wp_enqueue_style( 'slippery-slider-css' );
@@ -279,6 +484,6 @@ Domain Path: /languages
 		wp_enqueue_script( 'pegasus-slider-plugin-js' );
 
 		return '<section id="news-demo">' . $output . '</section>';
-	   
+
 	}
 	add_shortcode( "news_slider", "news_slider_query_shortcode" );
